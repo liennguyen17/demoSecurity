c3010339670e8cc27f7f2d455a23ab380bcf44c4
liennguyen17 <nguyenlienltw@gmail.com>
2023-03-29 00:22:37 +0700

    demoSecurity

---
+++ src/main/java/com/example/demosecurity/config/WebSecurityConfig.java
@@ -0,0 +1,68 @@
+package com.example.demosecurity.config;
+
+import com.example.demosecurity.config.JwtAuthenticationFilter;
+import com.example.demosecurity.constant.RoleEnum;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS;
+
+@Configuration  //Cấu hình trước khi load
+@EnableWebSecurity  //cho phép sử dụng security
+@RequiredArgsConstructor
+public class WebSecurityConfig {
+
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
+    private final AuthenticationProvider authenticationProvider;
+    @Bean
+    //SecurityFilterChain xác định đường dẫn URL nào đưa vào sẽ được bảo mật và đường nào thì không, ở dưới đường dẫn "/" và "/home" sẽ không yêu cầu xác thực
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {     //HttpSecurity: là tham số sẽ lắng nghe toàn bộ request
+        http.csrf().disable();
+        http.cors().disable();
+        http.sessionManagement().sessionCreationPolicy(STATELESS);
+        http.authorizeRequests()
+                .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
+                .requestMatchers("/api/permit/**").permitAll()
+                .requestMatchers("/home").authenticated()
+                .requestMatchers("/api/auth/delete").hasAnyAuthority("USER", "ADMIN", "SUPER_ADMIN")
+                .requestMatchers("/api/auth/manage/**").hasAnyAuthority("ADMIN", "SUPER_ADMIN")
+                .and()
+                .csrf().disable()
+                .authorizeRequests()
+                .anyRequest().authenticated()
+                .and()
+                .authenticationProvider(authenticationProvider)
+                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
+                .formLogin((form) -> form
+                .loginPage("/login")      // login sẽ được thực hiện ở trang có domain "/login"
+                .permitAll())
+                .logout((logout) -> logout
+                        .permitAll()
+                );
+        return http.build();
+    }
+
+//    @Bean
+//    //Bean này thiết lập kho lưu trữ người dùng trong bộ nhớ với một người dùng, người dùng này được cấp tên, mật khẩu, vai trò của người dùng
+//    public UserDetailsService userDetailsService(){
+//        UserDetails user = User.withDefaultPasswordEncoder()
+//                .username("Hoang")
+//                .password("hoang123")
+//                .roles("USER")
+//                .build();
+//        return new InMemoryUserDetailsManager(user);
+//    }
+}
